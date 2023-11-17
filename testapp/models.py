803d73744baf34cee2f62e023c7f089f80799d2b
Asliddin9220 <asliddinmalikov92@gmail.com>
2023-11-17 05:17:29 +0500

    first commit

--- testapp/models.py
+++ testapp/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Avto(models.Model):
-  model = models.CharField(max_length=30, null=True, blank=True)
-  price = models.IntegerField(default=10000)
-  color = models.CharField(max_length=20, default='oq')
-  year = models.CharField(max_length=30, default='2000')
-  image = models.ImageField(upload_to='myproject/static/images/', default='myproject/static/images/malibu.jpg')
-
-def __str__(self) -> str:
-   return self.model
